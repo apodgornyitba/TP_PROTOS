2566e829add9f26180a0fe26579966c14d547b96
sanselmo <soanselmo@itba.edu.ar>
2022-11-17 19:50:26 -0300

    fixes in files and rearange functions

---
+++ include/connecting.h
@@ -0,0 +1,28 @@
+#ifndef CONNECTING_H
+#define CONNECTING_H
+#include "selector.h"
+#include "debug.h"
+#include "socks5nio.h"
+#include "stm.h"
+#include "states.h"
+/**
+ * Etapa de conexión
+ * Actualmente se conecta a un destino fijo.
+ * Cuando termina pasa al estado COPY.
+ */
+/**
+ * @param state
+ * @param key
+ */
+void connecting_init(const unsigned state, struct selector_key *key);
+/**
+ * @param key
+ * @return
+ */
+unsigned connecting_write(struct selector_key *key);
+/**
+ * @param key
+ * @return
+ */
+unsigned connecting_read(struct selector_key *key);
+#endif
---
+++ include/states.h
@@ -0,0 +1,80 @@
+#ifndef STATES_H
+#define STATES_H
+#include "buffer.h"
+#include "hello.h"
+#include "request.h"
+
+// Definición de variables para cada estado
+
+/** Used by the HELLO_READ and HELLO_WRITE states */
+struct hello_parser parser;
+typedef struct hello_st
+{
+    /** Buffers used for IO */
+    buffer *rb, *wb;
+    /** Pointer to hello parser */
+    struct hello_parser parser;
+    /** Selected auth method */
+    uint8_t method;
+};
+
+/** Used by the USERPASS_READ and USERPASS_WRITE states */
+//typedef struct userpass_st
+//{
+/** Buffers used for IO */
+//buffer *rb, *wb;
+/** Pointer to hello parser */
+//struct up_req_parser parser;
+/** Selected user */
+//uint8_t * user;
+/** Selected password */
+//uint8_t * password;
+//};
+
+/** Used by the REQUEST_READ, REQUEST_WRITE and REQUEST_RESOLV state */
+enum socks5_response_status {
+    HOLA
+};
+typedef struct request_st
+{
+    buffer *rb, *wb;
+
+
+    struct request request;
+    struct request_parser parser;
+
+
+    enum socks5_response_status status;
+
+    struct sockaddr_storage *origin_addr;
+    socklen_t *origin_addr_len;
+    int *origin_domain;
+
+    const int *client_fd;
+    int *origin_fd;
+};
+
+/** Used by REQUEST_CONNECTING */
+typedef struct connecting{
+    buffer *wb;
+    const int *client_fd;
+    int *origin_fd;
+    enum socks_reply_status *status;
+};
+
+/** Used by the COPY state */
+typedef struct copy_st
+{
+    /** File descriptor */
+    int fd;
+    /** Reading buffer */
+    buffer * rb;
+    /** Writing buffer */
+    buffer * wb;
+    /** Interest of the copy */
+    fd_interest interest;
+    /** Pointer to the structure of the opposing copy state*/
+    struct copy * other_copy;
+};
+
+#endif
